560a04be5636bd3811965b900639870cad655e14
CodeRed30 <katrinaharradine@gmail.com>
2021-03-09 14:58:40 +0000

    Iterate through the transactions

--- lib/bank.rb
+++ lib/bank.rb
@@ -4,13 +4,21 @@ class Account
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount, date)
-    @balance += amount
+    amount = sprintf('%05.2f', amount)
+    @balance += amount.to_i
+    balance = sprintf('%05.2f', @balance)
+    @transactions.unshift([date, amount << " ", nil, balance])
   end
 
   def statement
-    "date || credit || debit || balance\n10/01/2012 || 1000.00 || || #{sprintf('%05.2f', @balance)}"
+    statement_text = "date || credit || debit || balance"
+    @transactions.each do |date, credit, debit, balance|
+      statement_text << "\n#{date} || #{credit}|| #{debit}|| #{balance}"
+    end
+    statement_text
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -8,11 +8,18 @@ describe Account do
   end
 
   it 'can deposit funds' do
-    expect(account.deposit(1000, '10/01/2012')).to eq(1000)
+    account.deposit(1000, '10/01/2012')
+    expect(account.balance).to eq(1000)
   end
 
   it 'can display statement' do
     account.deposit(1000, '10/01/2012')
     expect(account.statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00")
+  end
+
+  it 'iterates through deposits on statement' do
+    account.deposit(1000, '10/01/2012')
+    account.deposit(2000, '13/01/2012')
+    expect(account.statement).to eq("date || credit || debit || balance\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00")
   end
 end
